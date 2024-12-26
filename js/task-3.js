ed39c77b1b0cc9478fa1585e277eb5cb1c2e3b2b
alexdevsprint <alexdevsprint@gmail.com>
2024-12-26 11:58:45 +0200

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-  return parseInt(content) + (parseInt(padding) + parseInt(border)) * 2;
+  return parseFloat(content) + (parseFloat(padding) + parseFloat(border)) * 2;
 }
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
